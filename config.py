ff05c37fc3ef72ee168ea2070d3712c60480fdd9
LPELLITTERI <lmo0o@yahoo.com>
2023-02-14 17:45:25 -0500

    fixed  api key

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 #Weather API Key
-weather_api_key = "7a57dd024665984c63e9d9c27f221eb0"
+api_key = "7a57dd024665984c63e9d9c27f221eb0"
 # Geoapify API Key
 geoapify_key = "10681c38e94241e689e8e140e58f7634"
